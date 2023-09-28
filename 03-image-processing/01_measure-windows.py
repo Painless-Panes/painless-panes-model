e990a2aea894762b1aa1ceca744605e6c6c5660e
Andreas Copan <avcopan@gmail.com>
2023-09-27 22:10:47 -0400

    WIP save

--- 03-image-processing/01_measure-windows.py
+++ 03-image-processing/01_measure-windows.py
@@ -5,34 +5,15 @@ import painless_panes
 image = cv2.imread("example1.jpg")
 height, width, channels = image.shape
 
-width = int(width * 640 / height)
-height = int(height * 640 / height)
+width = int(width * 800 / height)
+height = int(height * 800 / height)
 
 image = cv2.resize(image, (width, height), interpolation=cv2.INTER_LINEAR)
 windows = painless_panes.model.get_detections(image)
 window = painless_panes.cv.select_central_detection(image, windows)
-x0, y0, x1, y1 = window["bounding_box"]
-# width, height, message = painless_panes.cv.measure_window(image, annotate=True)
+bbox = window["bounding_box"]
 
-# print(width, height, message)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.GaussianBlur(image, (5, 5), 0)
-# image = cv2.bilateralFilter(image, 9, 75, 75)
-
-all_edges = cv2.Canny(image, 30, 150)
-edges = numpy.zeros_like(all_edges)
-edges[y0:y1, x0:x1] = all_edges[y0:y1, x0:x1]
-
-fld = cv2.ximgproc.createFastLineDetector()
-# Get line vectors from the image
-lines = fld.detect(edges)
-# vlines, hlines = painless_panes.cv.partition_lines_by_orientation(lines)
-# Draw lines on the image
-print(repr(lines))
-print(lines.shape)
-image = fld.drawSegments(image, lines, linecolor=(0, 255, 0))
-# image = fld.drawSegments(image, hlines, linecolor=(255, 0, 0))
-
+image = painless_panes.cv.find_window_corners(image, bbox)
 
 # # Find the contours of the edges
 # contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -44,7 +25,6 @@ image = fld.drawSegments(image, lines, linecolor=(0, 255, 0))
 #     if len(polygon) == 4:
 #       cv2.drawContours(image, [polygon], -1, (0, 255, 0), 2)
 
-cv2.imshow("Blurred", image)
-cv2.imshow("Edges", edges)
+cv2.imshow("1", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
