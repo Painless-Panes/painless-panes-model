7b566aa66ede7eb7c4af9a323fdb1601e360f614
Andreas Copan <avcopan@gmail.com>
2023-10-06 16:17:44 -0400

    Testing model v3
    No noticeable improvement over v2

--- 02-model-training/03_predict.py
+++ 02-model-training/03_predict.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO("window-model-nano-v1.pt")
+model = YOLO("window-model-nano-v3.pt")
 
 results = model(
-    f"val/images/D_c_31.75x45.jpeg",
+    f"val/images/G_a_24x53-55x52.63-24x53.jpeg",
     show=True,
     conf=0.25,
     save=True,
